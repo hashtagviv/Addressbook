32eac34f8d387a21fece934ed59583f9b5cef154
Hashtagviv <vivekpsuresh@gmail.com>
2020-12-10 13:19:38 +0530

    changed placement of flask in requirements.txt

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     install_requires=[
         'arrow==0.13.0',
         'bs4==0.0.1',
-        'Flask==1.0.2',
         'Flask-Testing==0.7.1',
         'html5validator==0.3.1',
         'nodeenv==1.3.3',
